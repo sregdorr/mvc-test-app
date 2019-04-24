e3f5ab8cbb102deef585001bcc0a0a837fceadb1
Russ Rodgers <rrodgers@thebgoc.com>
2019-04-24 17:47:06 -0500

    Update controller

--- mvc-test-app/Controllers/HomeController.cs
+++ mvc-test-app/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace mvc_test_app.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Application description page.";
 
             return View();
         }
